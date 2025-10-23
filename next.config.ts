5448667b1e2f15b35c87e956778079e9bfcf1149
Doug Ross <122139227+RhinoBytes@users.noreply.github.com>
2025-10-23 10:55:17 -0400

    fixed dynamic building

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'c1.scryfall.com',
+        protocol: "https",
+        hostname: "c1.scryfall.com",
       },
       {
-        protocol: 'https',
-        hostname: 'cards.scryfall.io',
+        protocol: "https",
+        hostname: "cards.scryfall.io",
       },
     ],
   },
